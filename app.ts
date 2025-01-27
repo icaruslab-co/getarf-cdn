0ac4e889237b879ab8d785e7e4bf8bff7c5d1b6f
Berke <sygnaplay58@gmail.com>
2025-01-27 15:55:03 +0300

    updated

--- app.ts
+++ app.ts
@@ -5,9 +5,16 @@ import requestIp from 'request-ip';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
 
 const app: express.Application = express();
 const b_r: string = '/v' + pjson.version;
+var port = pjson.port;
+for (let i = 0; i < process.argv.length; i++) {
+    if (process.argv[i] == '--port')
+        if (process.argv.length >= i)
+            port = process.argv[i + 1];
+}
 
 app.use(cors());
 app.use(requestIp.mw());
@@ -15,15 +22,17 @@ app.use(bodyParser.json({ type: "application/json" }));
 app.use(morgan('tiny'), (req: Request, res: Response, next: NextFunction) => { console.log(req.clientIp); next(); });
 
 /* Routers */
-app.use(b_r, express.static('public')); // static files
+app.use(b_r, express.static('public'));
+console.log(__dirname);
+
 //
 
 app.use('*', async (req: Request, res: Response, next: NextFunction) => {
     res.status(400).end();
 });
 
-const listener = app.listen(process.argv[0] ? parseInt(process.argv[0]) : 4143, 'localhost', () => {
-    console.log("Server started ::" + (process.argv[0] ?? 4143));
+const listener = app.listen(parseFloat(port), 'localhost', () => {
+    console.log("Server started ::" + port);
 });
 
 process.stdout.write(
